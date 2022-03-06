65462c939e24c945b6def28b30d9d949f6f54a9d
Leslie Patino <lesliejpatino@gmail.com>
2022-03-06 09:43:11 -0800

    created starter files

---
+++ src/components/pages/About.js
@@ -0,0 +1,26 @@
+import React, { useState }  from 'react'
+// import styled from 'styled-components'
+// import App from '../App'
+// import Contact from './Contact'
+// import Portfolio from './Portfolio'
+// import Resume from './Resume'
+// import Nav from './Nav'
+
+// function About() {
+//     const [ , ] = useState([]);
+//     return (
+//         <Box> 
+
+//         </Box>
+//     )
+// }
+
+const About = () => {
+    const style={};
+    return (
+        <div className="container border">
+           <p>Hello, World</p> 
+        </div>
+    )
+}
+export default About()

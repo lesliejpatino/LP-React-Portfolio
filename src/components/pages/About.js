bece389c2552da6948851b3a63c63f70fd72b667
Leslie Patino <lesliejpatino@gmail.com>
2022-03-06 15:43:45 -0800

    created home/about page and added styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,26 +1,26 @@
-import React, { useState }  from 'react'
-// import styled from 'styled-components'
-// import App from '../App'
-// import Contact from './Contact'
-// import Portfolio from './Portfolio'
-// import Resume from './Resume'
-// import Nav from './Nav'
+import React from 'react'
+import '../../styling/About.css'
 
-// function About() {
-//     const [ , ] = useState([]);
-//     return (
-//         <Box> 
+export default function About() {
+    const headshot = process.env.PUBLIC_URL + '/assets/images/headshot.png';
 
-//         </Box>
-//     )
-// }
 
-const About = () => {
-    const style={};
     return (
-        <div className="container border">
-           <p>Hello, World</p> 
+        <div id="home-div">
+            <div id="about">
+                <h1>Hi,
+                    <br />
+                    I'm Leslie</h1>
+                <h3>full stack web developer</h3>
+                <hr />
+                <p id="bio">
+                    Glad you're here! Please have a look around and get in touch if you would like to work together. <br />
+                    My top value is growth and it drives everything I do! I thrive in an environment that requires equal parts creative and logical thinking - which is why I love web development. 
+                </p>
+            </div>
+                <img src={headshot} alt="leslies headshot" id="my-headshot"/>
         </div>
+
     )
-}
-export default About()
+};
+

d7ac30ba2fa8eaa0860eaef6afcb339283c7152b
Leslie Patino <lesliejpatino@gmail.com>
2022-03-07 10:27:46 -0800

    added links to each projects github repo

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -8,44 +8,79 @@ export default function Portfolio() {
     const notes = process.env.PUBLIC_URL + '/assets/images/note-site.png';
     const pwdGenerator = process.env.PUBLIC_URL + '/assets/images/pwdgen-site.png';
     const scheduler = process.env.PUBLIC_URL + '/assets/images/sched-site.png';
+    const gitHubLogo = process.env.PUBLIC_URL + '/assets/images/gh.png';
 
 
     return (
         <>
-        <div className="col-8 m-5">
-            <div className="row">
-            <div className="card col-5 mx-4 mb-4 border project-cards">
-                <a href="https://dry-badlands-77030.herokuapp.com/"><h4>Organizarr</h4></a>
-                <img className="project-images" src={organizarr} alt="org site" /> <br />
-            </div>
+            <div className="col-8 m-5 text-center">
+                <div className="row">
+                    <div className="card col-5 mx-4 mb-4 border bg-black border bg-black project-cards">
+                        <a href="https://dry-badlands-77030.herokuapp.com/" rel="noreferrer" target="_blank">
+                            <h4>Organizarr</h4>
+                        </a>
+                        <img className="project-images" src={organizarr} alt="org site" /> <br />
+                        <a href="https://github.com/lesliejpatino/Organizarr" rel="noreferrer" target="_blank">
+                            <img src={gitHubLogo} alt="github-logo" id="repo-link" />
+                        </a>
+                    </div>
 
-            <div className="card col-5 mx-4 mb-4 project-cards">
-            <a href="https://lesliejpatino.github.io/Find-Covid-Testing/"><h4>Map-A-Covid-Test</h4></a>
-                <img className="project-images" src={covid} alt="covid site" /> <br />
-            </div>
+                    <div className="card col-5 mx-4 mb-4 border bg-black project-cards">
+                        <a href="https://lesliejpatino.github.io/Find-Covid-Testing/" rel="noreferrer" target="_blank">
+                            <h4>Map-A-Covid-Test</h4>
+                        </a>
+                        <img className="project-images" src={covid} alt="covid site" /> <br />
+                        <a href="https://github.com/lesliejpatino/Find-Covid-Testing" rel="noreferrer" target="_blank">
+                            <img src={gitHubLogo} alt="github-logo" id="repo-link" />
+                        </a>
+                    </div>
 
-            <div className="card col-5 mx-4 mb-4 project-cards">
-            <a href="https://github.com/lesliejpatino/AllThingsTech-Blog"><h4>All Things Tech - Blog</h4></a>
-                <img className="project-images" src={blog} alt="blog site" /> <br />
-            </div>
+                    <div className="card col-5 mx-4 mb-4 border bg-black project-cards">
+                        <a href="https://github.com/lesliejpatino/AllThingsTech-Blog" rel="noreferrer" target="_blank">
+                            <h4>All Things Tech - Blog</h4>
+                        </a>
+                        <img className="project-images" src={blog} alt="blog site" /> <br />
+                        <a href="https://github.com/lesliejpatino/AllThingsTech-Blog" rel="noreferrer" target="_blank">
+                            <img src={gitHubLogo} alt="github-logo" id="repo-link" />
+                        </a>
 
-            <div className="card col-5 mx-4 mb-4 project-cards">
-            <a href="https://obscure-waters-08324.herokuapp.com/"><h4>Save A Note</h4></a>
-                <img className="project-images" src={notes} alt="notes site" /> <br />
-            </div>
+                    </div>
 
-            <div className="card col-5 mx-4 mb-4 project-cards">
-            <a href="https://lesliejpatino.github.io/Password-Generator/"><h4>Password Generator</h4></a>
-                <img className="project-images" src={pwdGenerator} alt="pwd site" /> <br />
-            </div>
+                    <div className="card col-5 mx-4 mb-4 border bg-black project-cards">
+                        <a href="https://obscure-waters-08324.herokuapp.com/" rel="noreferrer" target="_blank">
+                            <h4>Save A Note</h4>
+                        </a>
+                        <img className="project-images" src={notes} alt="notes site" /> <br />
+                        <a href="https://github.com/lesliejpatino/Note-Saver" rel="noreferrer" target="_blank">
+                            <img src={gitHubLogo} alt="github-logo" id="repo-link" />
+                        </a>
 
-            <div className="card col-5 mx-4 mb-4 project-cards">
-            <a href="https://lesliejpatino.github.io/Schedule-Your-Workday/"><h4>Work Day Scheduler</h4></a>
-                <img className="project-images" src={scheduler} alt="scheduler site" />
-            </div>
-            </div>
+                    </div>
+
+                    <div className="card col-5 mx-4 mb-4 border bg-black project-cards">
+                        <a href="https://lesliejpatino.github.io/Password-Generator/" rel="noreferrer" target="_blank">
+                            <h4>Password Generator</h4></a>
+                        <img className="project-images" src={pwdGenerator} alt="pwd site" /> <br />
+                        <a href="https://github.com/lesliejpatino/Password-Generator" rel="noreferrer" target="_blank">
+                            <img src={gitHubLogo} alt="github-logo" id="repo-link" />
+                        </a>
+
+
+                    </div>
+
+                    <div className="card col-5 mx-4 mb-4 border bg-black project-cards">
+                        <a href="https://lesliejpatino.github.io/Schedule-Your-Workday/" rel="noreferrer" target="_blank">
+                            <h4>Work Day Scheduler</h4>
+                        </a>
+                        <img className="project-images" src={scheduler} alt="scheduler site" />
+                        <a href="https://github.com/lesliejpatino/Schedule-Your-Workday" rel="noreferrer" target="_blank">
+                            <img src={gitHubLogo} alt="github-logo" id="repo-link" />
+                        </a>
+                    </div>
+
+                </div>
             </div>
         </>
-        
+
     );
 };

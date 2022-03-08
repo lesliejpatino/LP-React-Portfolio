e076d7337dd64f16d5486ffd59f464d5dc0aba71
Leslie Patino <lesliejpatino@gmail.com>
2022-03-07 18:39:37 -0800

    refactored the nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,60 +4,55 @@ import "../styling/Nav.css";
 function Nav({ currentPage, handlePageChange }) {
   const style = {
     link: {
-      color: "white"
-      // font-weight: "bolder",
+      color: "grey",
     }
   }
   return (
     <>
-      <div className="col-12 m-1">
-        <div className="row justify-content-between">
-          <h2 className="col-3 p-3 mx-3" id="header-name">Leslie Patino</h2>
-          <ul className="nav col-8 p-2 mx-3">
-            <li className="nav-item">
-              <a
-                href="#about"
-                onClick={() => handlePageChange("About")}
-                className={currentPage === "About" ? "nav-link active" : "nav-link"}
-                style={style.link}
-              >
-                About Me
-              </a>
-            </li>
+      <div class="col-12 py-2">
+        <div class="row">
+          <div className="col-3">
+            <p className="mx-4" id="my-name">
+              Leslie Patino
+            </p>
+          </div>
+          <div className="col-9">
 
-            <li className="nav-item">
-              <a
-                href="#portfolio"
-                onClick={() => handlePageChange("Portfolio")}
-                className={
-                  currentPage === "Portfolio" ? "nav-link active" : "nav-link"
-                }
-                style={style.link}
-              >
-                Portfolio
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#contact"
-                onClick={() => handlePageChange("Contact")}
-                className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
-                style={style.link}
-              >
-                Contact Me
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#resume"
-                onClick={() => handlePageChange("Resume")}
-                className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
-                style={style.link}
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
+            <ul className='nav nav-tabs'>
+
+              <li className='nav-item'>
+                <a href='#about' onClick={() => handlePageChange('About')}
+                  className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                  style={style.link}>
+                  About Me
+                </a>
+              </li>
+
+              <li className='nav-item'>
+                <a href='#portfolio' onClick={() => handlePageChange('Portfolio')}
+                  className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                  style={style.link}>
+                  Portfolio
+                </a>
+              </li>
+
+              <li className='nav-item'>
+                <a href='#contact' onClick={() => handlePageChange('Contact')}
+                  className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                  style={style.link}>
+                  Contact
+                </a>
+              </li>
+
+              <li className='nav-item'>
+                <a href='#contact' onClick={() => handlePageChange('Resume')}
+                  className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                  style={style.link}>
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
